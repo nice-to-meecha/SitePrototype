e0ad841d5dcdf3d56a3aa8d7ff32c3498653fffa
Stamesha Bello <stameshabello@gmail.com>
2024-09-18 03:46:44 -0700

    Layout not included in last commit

---
+++ test-anims/src/Test.jsx
@@ -0,0 +1,18 @@
+import './Test.css'
+
+function App() {
+
+    return (
+      <>
+        <div className='header'/>
+        <div className='opening-text'/>
+        <div className='service-intros'/>
+        <div className='certs'/>
+        <div className='reviews'/>
+        <div className='call-to-action'/>
+        <div className='footer'/>
+      </>
+    )
+}
+
+export default App
