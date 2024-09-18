aed134871da13ae9242c3edff8769958eb5685c6
Stamesha Bello <stameshabello@gmail.com>
2024-09-18 11:56:35 -0700

    Looping video

--- test-anims/src/Test.jsx
+++ test-anims/src/Test.jsx
@@ -4,7 +4,17 @@ function App() {
 
     return (
       <>
-        <div className='header'/>
+        <div className='header'>
+          {/* Include multiple sources with different file formats,
+            for varying browser support */}
+          <video className='header-video' autoPlay controls loop muted>
+            <source
+              src='/test-stock-video.mp4'
+              type='video/mp4'
+            />
+            Video giving meaningful introduction to Blantyre Business Services
+          </video>
+        </div>
         <div className='opening-text'/>
         <div className='service-intros'/>
         <div className='certs'/>
