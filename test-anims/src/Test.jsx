5563b10ed0045d475ebbac8a13d5cf79f5502882
Stamesha Bello <stameshabello@gmail.com>
2024-09-20 13:32:31 -0700

    Fade-in text on scroll

--- test-anims/src/Test.jsx
+++ test-anims/src/Test.jsx
@@ -1,21 +1,43 @@
+import { useEffect, useRef } from 'react'
 import './Test.css'
 
 function App() {
+
+  const fadeInTextRef = useRef(null);
+  let fadeInTextObserver;
+
+  useEffect(() => {
+    fadeInTextObserver = new IntersectionObserver((entries, observer) => {
+      entries.forEach(el => {
+        if (el.isIntersecting) {
+          el.target.classList.add("show");
+          observer.unobserve(el.target);
+        }
+      })
+    }, {
+      threshold: 1.0,
+      rootMargin: "-10% 0%"
+    });
+   fadeInTextObserver.observe(fadeInTextRef.current);
+  })
 
     return (
       <>
         <div className='header'>
           {/* Include multiple sources with different file formats,
             for varying browser support */}
-          <video className='header-video' autoPlay controls loop muted>
+          <video className='header-video' autoPlay loop muted>
             <source
               src='/test-stock-video.mp4'
               type='video/mp4'
             />
             Video giving meaningful introduction to Blantyre Business Services
           </video>
+          <div className='video-transition'></div>
         </div>
-        <div className='opening-text'/>
+        <div className='opening-text'>
+          <p ref={fadeInTextRef}>We are here to show you a better way</p>
+        </div>
         <div className='service-intros'/>
         <div className='certs'/>
         <div className='reviews'/>
